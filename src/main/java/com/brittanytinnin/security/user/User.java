f140f44aaedf86600e2098f313d38e718f5119ba
Brittany Tinnin <bntinnin7@gmail.com>
2023-01-11 10:25:54 -0700

    first-commit

---
+++ src/main/java/com/brittanytinnin/security/user/User.java
@@ -0,0 +1,13 @@
+package com.brittanytinnin.security.user;
+
+
+
+public class User {
+
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+
+}
